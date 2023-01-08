7487229befe78ce637f3c6c394c8dff0738e79d8
Rebekka Nordheim <111970970+rebekkanordheim@users.noreply.github.com>
2023-01-08 14:39:59 +0100

    spørsmål besvart

--- js/script1.js
+++ js/script1.js
@@ -1,4 +1,60 @@
-var integer = 8;
-var decimal = 7.1;
+// Q1
+var integer = 5;
+var decimal = 3.4;
 
-console.log(integer + decimal)
+
+// Q2 
+var daysOfTheWeek = "Monday" + "-" + "Tuesday" + "-" + "Wednesday" + "-" + "Thursday" + "-" + "Friday" ;
+
+
+// Q3 
+var addition = 3 + 5;
+var subtraction = 10 - 7;
+var multiplication = 2 * 10;
+var division = 20 / 4;
+
+// Q4
+var loggedIn = false;
+
+// Q5 
+var sentence = "The frogs are angry.";
+console.log(sentence.length);
+
+// Q6
+var timeElapsed;
+var totalPrice;
+
+// Q7
+var number1 = "53";
+var number2 = "44.6";
+
+var convertedNumber1 = parseInt(number1);
+var convertedNumber2 = parseFloat(number2);
+
+// Q8
+var pet = "donkey";
+var age = 89;
+var discountApplied = "true";
+var timeLeft;
+
+var typeOfPet = typeof pet;
+console.log(typeOfPet);
+
+var typeOfAge = typeof age;
+console.log(typeOfAge);
+
+var typeOfDiscountApplied = typeof discountApplied;
+console.log(typeOfDiscountApplied);
+
+var typeOfTimeLeft = typeof timeLeft;
+console.log(typeOfTimeLeft);
+
+// Q9
+var headingText = "Updating heading";
+
+var heading = document.querySelector("h1");
+heading.innerHTML = headingText;
+
+// Q10
+var body = document.querySelector("body");
+body.style.backgroundColor = "lightskyblue";
