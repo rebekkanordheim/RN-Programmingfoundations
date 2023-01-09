1ec57e6a22c4f3a298b66c007df12c75409da8df
Rebekka Nordheim <111970970+rebekkanordheim@users.noreply.github.com>
2023-01-09 19:17:14 +0100

    Making decisions questions

--- js/script2.js
+++ js/script2.js
@@ -1,1 +1,22 @@
 console.log("This is script 2");
+
+// Lesson task in making decisions
+// Q1
+var firstNumber = 34;
+var secondNumber = 89;
+
+if (firstNumber < secondNumber) {
+    console.log("The first number is smaller than the second");
+}
+
+// Q2
+var age = 11;
+var minimumAge = "13";
+
+if (age >= parseInt(minimumAge)) {
+    console.log("Minimum age met");
+} else {
+    console.log("Minimum age not met");
+}
+
+// Q3
