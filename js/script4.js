30d42253796ad0b2429a39d25798545c096a09e0
Rebekka Nordheim <111970970+rebekkanordheim@users.noreply.github.com>
2023-01-12 13:38:35 +0100

    Humphrey done

--- js/script4.js
+++ js/script4.js
@@ -1,2 +1,10 @@
 console.log("This is script 4");
 
+var firstName = "Humphrey";
+var lengthOfName = firstName.length;
+console.log(lengthOfName);
+
+var lower = firstName.toLowerCase();
+console.log(lower);
+var lower = firstName.toUpperCase();
+console.log(lower);
