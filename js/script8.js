b74818c1591be20b32514284339c1507cf641a16
Rebekka Nordheim <111970970+rebekkanordheim@users.noreply.github.com>
2023-01-20 12:02:59 +0100

    Q4

--- js/script8.js
+++ js/script8.js
@@ -26,4 +26,23 @@ function multiply(firstNumber, secondNumber) {
 var result = multiply(10, 4);
 console.log(result);
 
-//
+//Q4
+function addition(num1, num2, num3) {
+    // try and convert all the arguments to number values
+    var convertedNum1 = parseFloat(num1);
+    var convertedNum2 = parseFloat(num2);
+    var convertedNum3 = parseFloat(num3);
+    // if any of the arguments cannot be converted to a number they will have the value of NaN - Not a number
+    //check for a NaN value using isNaN()
+    if (isNaN(convertedNum1) || isNaN(convertedNum2) || isNaN(convertedNum3)) {
+        // as soon as a return statement is encountered the function will exit
+        return "Invalig argument types";
+    }
+    return convertedNum1 + convertedNum2 + convertedNum3;
+}
+// select the p element
+var paragraph = document.querySelector("p");
+
+var sum = addition(6, true, "f");
+
+paragraph.innerHTML = sum;
