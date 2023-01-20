52245ada8eb37e0a859e98551d8b6cb9ed471b51
Rebekka Nordheim <111970970+rebekkanordheim@users.noreply.github.com>
2023-01-20 13:58:18 +0100

    questions done

--- js/script8.js
+++ js/script8.js
@@ -32,17 +32,59 @@ function addition(num1, num2, num3) {
     var convertedNum1 = parseFloat(num1);
     var convertedNum2 = parseFloat(num2);
     var convertedNum3 = parseFloat(num3);
-    // if any of the arguments cannot be converted to a number they will have the value of NaN - Not a number
-    //check for a NaN value using isNaN()
+
+    // if any of the arguments cannot be converted to a number they will have the value NaN - Not a Number
+    // check for a NaN value using isNaN()
     if (isNaN(convertedNum1) || isNaN(convertedNum2) || isNaN(convertedNum3)) {
         // as soon as a return statement is encountered the function will exit
-        return "Invalig argument types";
+        return "Invalid argument types";
     }
+
     return convertedNum1 + convertedNum2 + convertedNum3;
+
 }
+
 // select the p element
 var paragraph = document.querySelector("p");
 
 var sum = addition(6, true, "f");
 
 paragraph.innerHTML = sum;
+
+//Q5 
+var buttonHeading = document.querySelector(".heading");
+
+buttonHeading.onclick = function () {
+    var heading = document.querySelector("h1");
+    heading.innerHTML += ": Updated";
+};
+
+//Q6 
+var buttonTitle = document.querySelector(".title");
+
+function changeTitle() {
+    document.title = "I've been updated";
+}
+
+buttonTitle.onclick = changeTitle;
+
+//Q7
+var redButton = document.querySelector(".red");
+var orangeButton = document.querySelector(".orange");
+var pinkButton = document.querySelector(".pink");
+//this function will be called by the other functions
+function changeBackground(colour) {
+    document.body.style.backgroundColor = colour;
+}
+
+redButton.onclick = function () {
+    changeBackground("red");
+};
+
+orangeButton.onclick = function () {
+    changeBackground("orange");
+};
+
+pinkButton.onclick = function () {
+    changeBackground("pink");
+};
